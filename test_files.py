82179edb90198e63e0bf294a3d5f6239c4d46a69
Sireesh Kodali <sireesh@siruknown.net>
2026-01-15 17:13:33 +0530

    test_files: Fix silly mistakes

--- test_files.py
+++ test_files.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 import typesense
 from tqdm import tqdm
+import json
 
 client = typesense.Client({
   'nodes': [{
@@ -20,12 +21,18 @@ def main():
 
   args = parser.parse_args()
 
-  state_code = Path("args.files_path").parents[0].name
+  state_code = Path(args.files_path).parents[0].name
+  missing_files = []
+
   for file_name in tqdm(Path(args.files_path).iterdir()):
     try:
-      client.collections['legislature_debates'].documents[f"{state_code}-{file_name}"].retrieve()
+      client.collections['legislature_debates'].documents[f"{state_code}-{file_name.name}"].retrieve()
     except Exception as e:
       print(e)
+      missing_files.append((state_code, file_name.name, e))
+
+  with open("errors.json", "w") as f:
+    json.dump(missing_files, f)
 
 
 if __name__ == "__main__":
